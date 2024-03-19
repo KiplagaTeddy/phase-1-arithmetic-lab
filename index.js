ca1fedff8d87f2d96ade0beb65f609480c079d18
Teddy <teddykipchumba17@gmail.com>
2024-03-19 10:20:49 +0300

    arithmetics lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+let multiply=num1*num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 0) + 1
+
+let num3=9
+let num4=85
+let mod = num4%num3
+
+let max = Math.max(19, 4, 20, 1, 12);
